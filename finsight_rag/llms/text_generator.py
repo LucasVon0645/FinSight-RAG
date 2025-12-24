c0dddf6525bd45d07023735e85720ac0e7898f90
LucasVon0645 <lucas.ancken@usp.br>
2025-12-23 21:52:24 -0300

    Add support for Gemini and remote Hugging Face models in text generator
    - Introduced `build_gemini_text_gen_llm` function for Gemini model integration using Google API.
    - Added `build_hf_remote_text_gen_llm` function to utilize Hugging Face Inference API for remote model access.
    - Updated imports to include necessary classes from LangChain and utility functions for API key retrieval.
    - Refactored existing `build_hf_llm` function to `build_hf_local_text_gen_llm` for clarity.

--- finsight_rag/llms/text_generator.py
+++ finsight_rag/llms/text_generator.py
@@ -1,10 +1,17 @@
 from transformers import AutoTokenizer, AutoModelForCausalLM, pipeline
 
 # LangChain wrapper for HF pipeline (newer package)
-from langchain_huggingface import HuggingFacePipeline
+from langchain_huggingface import HuggingFacePipeline, HuggingFaceEndpoint, ChatHuggingFace
+from langchain_google_genai import ChatGoogleGenerativeAI
+
+from finsight_rag.utils import get_gemini_api_key, get_hf_api_key
 
 
-def build_hf_llm(model_id: str, max_new_tokens: int = 400, temperature: float = 0.2):
+
+def build_hf_local_text_gen_llm(model_id: str, max_new_tokens: int = 400, temperature: float = 1.0):
+    """
+    Returns a LangChain ChatModel compatible with LCEL pipes.
+    """
     tokenizer = AutoTokenizer.from_pretrained(model_id, use_fast=True)
     model = AutoModelForCausalLM.from_pretrained(
         model_id,
@@ -22,3 +29,46 @@ def build_hf_llm(model_id: str, max_new_tokens: int = 400, temperature: float = 
     )
 
     return HuggingFacePipeline(pipeline=gen_pipe)
+
+def build_gemini_text_gen_llm(
+    model_id: str = "gemini-2.5-flash",
+    temperature: float = 1.0,
+    max_new_tokens: int = 400,
+):
+    """
+    Returns a LangChain ChatModel compatible with LCEL pipes.
+    """
+    gemini_api_key = get_gemini_api_key()
+    return ChatGoogleGenerativeAI(
+        model=model_id,
+        google_api_key=gemini_api_key,
+        temperature=temperature,
+        max_output_tokens=max_new_tokens,
+    )
+
+def build_hf_remote_text_gen_llm(
+    model_id: str = "Qwen/Qwen2.5-7B-Instruct",
+    temperature: float = 0.0,
+    max_new_tokens: int = 400,
+):
+    """
+    Returns a LangChain LLM compatible with LCEL pipes
+    using Hugging Face Inference API.
+    """
+    hf_token = get_hf_api_key()
+    if not hf_token:
+        raise RuntimeError("HUGGINGFACEHUB_API_TOKEN is not set")
+
+    llm = HuggingFaceEndpoint(
+        repo_id=model_id,
+        temperature=temperature,
+        max_new_tokens=max_new_tokens,
+        huggingfacehub_api_token=hf_token,
+        task="text-generation",
+        provider="auto",
+    )
+    
+    return ChatHuggingFace(llm=llm)
+
+    
+    
