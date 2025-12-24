1161501b4df21e38c95dedbffd928e3f3f1ec731
LucasVon0645 <lucas.vongarcia@gmail.com>
2025-12-24 15:54:30 -0300

    feat: Refactor API key retrieval to use resource management for better portability

--- finsight_rag/utils.py
+++ finsight_rag/utils.py
@@ -1,4 +1,6 @@
 import yaml
+from importlib import resources as impresources
+import finsight_rag.config as config
 
 def load_yaml(file_path: str) -> dict:
     """Load a YAML file and return its contents as a dictionary."""
@@ -11,12 +13,13 @@ def load_data_config(config_path: str) -> dict:
 
 def get_gemini_api_key() -> str:
     """Retrieve the Gemini API key from file."""
-    file_path = "C:\\Users\\User\\projects\\FinSight-RAG\\gemini_api_key"
+    file_path = str(impresources.files(config) / "gemini_api_key")
     with open(file_path, 'r', encoding='utf-8') as file:
         return file.read().strip()
 
 def get_hf_api_key() -> str:
     """Retrieve the Hugging Face API key from file."""
-    file_path = "C:\\Users\\User\\projects\\FinSight-RAG\\hf_api_key"
+    
+    file_path = str(impresources.files(config) / "hf_api_key")
     with open(file_path, 'r', encoding='utf-8') as file:
         return file.read().strip()
